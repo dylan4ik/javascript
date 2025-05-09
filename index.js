afe302cf24a9b590bb802427a0458fb867d53066
dylan4ik <129986777+dylan4ik@users.noreply.github.com>
2025-05-10 00:01:07 +0200

    .

---
+++ index.js
@@ -0,0 +1,18 @@
+/* const message = "Javascript";
+
+var x = 5;
+
+let x = 5;
+
+const x = 5;
+
+alert = message;
+
+const hotelName = prompt(hello)
+console.log(hotelName)
+ */ 
+
+const yourname = prompt("enter your name please");
+console.log(yourname)
+const yourage = prompt("please enter your age")
+console.log(yourage)
